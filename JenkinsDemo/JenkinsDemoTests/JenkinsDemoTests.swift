5e65e13df841668def582da34125ebe2a8ea7322
Aditya Sharma <aditya.s@vserv.com>
2023-02-08 17:11:32 +0530

    Adding Unit Tests

---
+++ JenkinsDemo/JenkinsDemoTests/JenkinsDemoTests.swift
@@ -0,0 +1,24 @@
+//
+//  JenkinsDemoTests.swift
+//  JenkinsDemoTests
+//
+//  Created by Admin_Vserv on 08/02/23.
+//
+
+import XCTest
+
+class JenkinsDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+}
